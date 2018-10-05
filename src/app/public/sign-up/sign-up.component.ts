a4896893ae680305e28bbe26e6840c234a2cdb68
azkarmoulana <azkar.moulana@gmail.com>
2018-10-05 23:02:07 +0530

    sign up methods

--- src/app/public/sign-up/sign-up.component.ts
+++ src/app/public/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+
+  constructor() {
+
+    this.signUpForm = new FormGroup({
+      email: new FormControl(null, Validators.email),
+      fname: new FormControl(null, Validators.required),
+      lname: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      cnfpass: new FormControl(null, this.passValidator)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  passValidator(control: AbstractControl) {
+    if (control && (control.value !== null || control.value !== undefined)) {
+      const cnfpassValue = control.value;
+
+      const passControl = control.root.get('password');
+      if (passControl) {
+        const passValue = passControl.value;
+        if (passValue !== cnfpassValue || passValue === '') {
+          return {
+            isError: true
+          };
+        }
+      }
+    }
+
+    return null;
   }
 
 }
