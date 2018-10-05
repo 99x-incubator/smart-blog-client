a4e5efc22f97544ad7d018bfc5c913179c6d72a8
azkarmoulana <azkar.moulana@gmail.com>
2018-10-06 00:21:29 +0530

    'userservice'

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  signupUser(body:any){
+    return this._http.post('http://localhost:3000/users/signup', body,{
+      observe:'body'
+    });
+  }
+
 }
