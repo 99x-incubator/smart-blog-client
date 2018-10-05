a24f48d193142109ae59dcc7461bddec3a08a34d
Malinda28 <malindaranasinghe28@gmail.com>
2018-10-06 04:12:12 +0530

    login service half-way done

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,13 +5,20 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserService {
+  private api;
+  constructor(private _http: HttpClient) {
+    this.api = 'http://localhost:3000/';
+  }
 
-  constructor(private _http: HttpClient) { }
-
-  signupUser(body:any){
-    return this._http.post('http://localhost:3000/users/signup', body,{
-      observe:'body'
+  signupUser(body: any) {
+    return this._http.post(this.api + 'users/signup', body, {
+      observe: 'body'
     });
   }
 
+  loginUser(body) {
+    return this._http.post(this.api + 'users/login', body, {
+      observe: 'body'
+    });
+  }
 }
