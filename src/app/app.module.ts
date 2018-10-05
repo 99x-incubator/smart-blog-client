a4e805409d7ddad334ea347d0b71a5223375bb06
RaveenDharmasiri <raveen.dharmasiri@gmail.com>
2018-10-05 21:53:29 +0530

    routes, halfway done sign in logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './public/sign-in/sign-in.component';
+
+const router: Routes = [
+  { path: 'signin' , component: SignInComponent},
+
+  { path: '' , redirectTo: '/signin' , pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -18,6 +25,7 @@ import { SignInComponent } from './public/sign-in/sign-in.component';
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent],
