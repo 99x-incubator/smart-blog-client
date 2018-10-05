5f0ae7514c34a7e416a70cde20d556591b0fd1b5
azkarmoulana <azkar.moulana@gmail.com>
2018-10-06 00:05:55 +0530

    'signup-ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
 import { SignUpComponent } from './public/sign-up/sign-up.component';
 import { TopNavComponent } from './shared/top-nav/top-nav.component';
 import { HomeComponent } from './public/home/home.component';
@@ -37,6 +38,8 @@ const router: Routes = [
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot(router)
   ],
   providers: [],
--- src/app/public/sign-up/sign-up.component.ts
+++ src/app/public/sign-up/sign-up.component.ts
@@ -61,4 +61,5 @@ export class SignUpComponent implements OnInit {
   }
 
 
+
 }
